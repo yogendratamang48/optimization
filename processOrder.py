9e31eb22e0f7e9c232d8cd82bc4d8d80aa494b76
Yogendra Tamang <48yogen@gmail.com>
2018-03-31 20:29:18 +0545

    add process order and folder for data

--- processOrder.py
+++ processOrder.py
@@ -1,155 +1,128 @@
-from ortools.constraint_solver import pywrapcp
-import pudb
-# pudb.set_trace()
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-def main():
-    # Create Solver
-    solver = pywrapcp.Solver("ProcessOrder")
+from collections import namedtuple
+# Values == requiredTime
+# weights =  deadline
+Item = namedtuple("Order", ['index','requiredTime', 'deadline'])
 
-    # Data Feed
-    machines_count = 1
-    all_machines = range(0, machines_count)
+def solve(input_data):
+    # Modify this code to run your optimization algorithm
 
-    # ["Knife", 5] means it takes 5 minutes to setup machine M1 for production
-    # of "Knife"
-    all_products = [["Knife", 3],
-                    ["Spoon", 2],
-                    ["Fork", 5]]
+    # parse the input
+    lines = input_data.split('\n')
 
-    # 30, [1,5] means 5 unit time is needed for product 1 on deadline 30th.
-    all_orders = [[60,[[0, 5], [2, 6]]],
-                  [90,[[0, 15], [1, 10], [2,10]]],
-                  [70,[[0, 7],[2, 4]]],
-                  [25,[[1, 7],[2, 8]]],
-                  [30,[[0, 9],[1, 5],[2, 2]]],
-                 ]
-    machines = [[0, 0],
-               [0, 0, 0],
-               [0, 0],
-               [0, 0],
-               [0, 0, 0],
-               ]
-
-    products = [[0, 2],
-                [0, 1, 2],
-                [0, 2],
-                [1, 2],
-                [0, 1, 2]
-               ]
-    setup_times = [3, 2, 5]
-
-    orders = []
-    # Separate product processing times
-    processing_times = []
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+    requiredTimes = []
     deadlines = []
-    for i, order in enumerate(all_orders):
-        productwise_time = []
-        for j, products in enumerate(order[1]):
-            productwise_time.append(products[1])
-        processing_times.append(productwise_time)
-        deadlines.append(order[0])
-        orders.append(order[1])
 
-    print("Problem Information".center(40, "*"))
-    print("Orders: ".center(20, '='))
-    for i, order in enumerate(orders):
-        print("Order: {0}".format(i), order[0:])
-    print("Processing Times: ".ljust(20), processing_times)
-    print("Deadlines ".ljust(20), deadlines)
+    items = []
 
-    #Statistics
-    num_machines = len(all_machines)
-    num_products = len(all_products)
-    num_orders = len(all_orders)
+    for i in range(1, item_count+1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+        requiredTimes.append(int(parts[0]))
+        deadlines.append(int(parts[1]))
 
-    print("Number of Machines: ".ljust(20), num_machines)
-    print("Number of Products: ".ljust(20), num_products)
-    print("Number of Orders: ".ljust(20), num_orders)
+    # a trivial greedy algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+    # value = 0
+    # weight = 0
+    # taken = [0]*len(items)
+    # update values to give priority to least deadlines
+    # values = [(capacity-value) for value in deadlines]
 
-    # Compute horizon
-    horizon = 0
-    for i in range(num_orders):
-        horizon += sum(processing_times[i])
-    print("Horizon : ".ljust(20), horizon)
-    print("Problem Information".center(40, "*"))
-
-    # Creating TASKS
-    # task(i, j) = represents i-th order and j-th product task
-    all_tasks = {}
-    for i, order in enumerate(orders):
-        for j, product in enumerate(order):
-            all_tasks[(i, j)] = solver.FixedDurationIntervalVar(0,
-                                                                deadlines[i],
-                                                                processing_times[i][j],
-                                                                False,
-                                                                'Order_%i_Product_%i' % (i, j)
-                                                               )
-    # Create Sequence Variables
-    all_sequences = []
-    all_machine_jobs = []
-    machine_jobs = []
-    for i in all_machines:
-        machine_jobs = []
-        for j, order in enumerate(orders):
-            for k, product in enumerate(order):
-                if machines[j][k] == i:
-                    machine_jobs.append(all_tasks[(j, k)])
-        disj = solver.DisjunctiveConstraint(machine_jobs, 'Machine %i'%i)
-        all_sequences.append(disj.SequenceVar())
-        solver.Add(disj)
-    print("Machine Jobs".center(40, "*"))
-    print(machine_jobs)
-    print("Sequences: ")
-    print(all_sequences)
-    # Setting Objective
-    # To minimize setup lengh of 
-    # Create search phases.
-    sequence_phase = solver.Phase([all_sequences[i] for i in all_machines],
-                                solver.SEQUENCE_DEFAULT)
-    main_phase = solver.Compose([sequence_phase])
-  # Create the solution collector.
-    collector = solver.LastSolutionCollector()
-
-  # Add the interesting variables to the SolutionCollector.
-    collector.Add(all_sequences)
-
-    for i in all_machines:
-        sequence = all_sequences[i];
-        sequence_count = sequence.Size();
-        for j in range(0, sequence_count):
-            t = sequence.Interval(j)
-            collector.Add(t.StartExpr().Var())
-            collector.Add(t.EndExpr().Var())
-      # Solve the problem.
-    disp_col_width = 10
-
-    for i in all_machines:
-        seq = all_sequences[i]
-        sol_line = "Machine " + str(i) + ": "
-        sol_line_tasks = "Machine " + str(i) + ": "
-        sequence = collector.ForwardSequence(0, seq)
-        seq_size = len(sequence)
-
-        for j in range(0, seq_size):
-            t = seq.Interval(sequence[j]);
-         # Add spaces to output to align columns.
-            sol_line_tasks +=  t.Name() + " " * (disp_col_width - len(t.Name()))
-
-        for j in range(0, seq_size):
-            t = seq.Interval(sequence[j]);
-            sol_tmp = "[" + str(collector.Value(0, t.StartExpr().Var())) + ","
-            sol_tmp += str(collector.Value(0, t.EndExpr().Var())) + "] "
-            # Add spaces to output to align columns.
-            sol_line += sol_tmp + " " * (disp_col_width - len(sol_tmp))
-
-        sol_line += "\n"
-        sol_line_tasks += "\n"
-
-    print(sol_line_tasks)
-    print("Time Intervals for Tasks\n")
-    print(sol_line)
+    [value, taken] = ProcessOrder( deadlines, requiredTimes, capacity)
     
-if __name__=='__main__':
-    main()
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
 
+def zeros(rows, cols):
+    zeroList=[]
+    for i in range(0, rows):
+        tmpList = []
+        for j in range(0, cols):
+            tmpList.append(0)
+        zeroList.append(tmpList)
+    return zeroList
+
+def getUsedItems(w, c):
+    i=len(c)-1
+    currentW = len(c[0])-1
+    marked = []
+    for i in range(i+1):
+        marked.append(0)
+    while (i>=0 and currentW>=0):
+        if (i==0 and c[i][currentW] >0) or c[i][currentW] != c[i-1][currentW]:
+            marked[i] = 1
+            currentW = currentW-w[i]
+        i=i-1
+    return marked
+
+def ProcessOrder(d, r, K):
+    n = len(d)
+    c=zeros(n, K+1)
+    for i in range(n): # i in [0, 1] if there are two items
+        for j in range(K+1): # j in [0, 1, 2...11] if knapsack is of size 11
+            if r[i]<=j:
+                c[i][j]=max(r[i]+c[i-1][j-r[i]], c[i-1][j])
+            else:
+                c[i][j]=c[i-1][j]
+    return [c[n-1][K], getUsedItems(r,c)]
+
+def plot_diagram():
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    data = [[1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 2, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 3, 0, 3]]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_aspect(1)
+
+    def avg(a, b):
+        return (a + b) / 2.0
+
+    for y, row in enumerate(data):
+        for x, col in enumerate(row):
+            x1 = [x, x+1]
+            y1 = [0, 0]
+            y2 = [1, 1]
+            if col == 1:
+                plt.fill_between(x1, y1, y2=y2, color='red')
+                plt.text(avg(x1[0], x1[1]), avg(y1[0], y2[0]), "A", 
+                        horizontalalignment='center',
+                        verticalalignment='center')
+            if col == 2:
+                plt.fill_between(x1, y1, y2=y2, color='orange')
+                plt.text(avg(x1[0], x1[0]+1), avg(y1[0], y2[0]), "B", 
+                        horizontalalignment='center',
+                        verticalalignment='center')
+            if col == 3:
+                plt.fill_between(x1, y1, y2=y2, color='yellow')
+                plt.text(avg(x1[0], x1[0]+1), avg(y1[0], y2[0]), "C", 
+                        horizontalalignment='center',
+                        verticalalignment='center')
+
+    plt.ylim(1, 0)
+    plt.show()
+    
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        print(solve(input_data))
+    else:
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/order_4)')
+    plot_diagram()
 
